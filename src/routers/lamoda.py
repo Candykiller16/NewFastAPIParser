dd13c9cda57cd3c9e468cf5d26408d4e83a65d19
Candykiller16 <Candykiller16@mail.ru>
2022-09-19 14:17:25 +0300

    added function get one sneaker

--- src/routers/lamoda.py
+++ src/routers/lamoda.py
@@ -11,6 +11,11 @@ router = APIRouter(
 )
 
 
-@router.get('/get-list', response_model=List[Sneakers])
+@router.get('/get-list', response_model=List[Sneakers], description="Get list of sneakers")
 def get_sneakers_list() -> List[Sneakers]:
     return container_controller.lamoda.get_list()
+
+
+@router.get('/get/{_id}', response_model=Sneakers, description="Get one sneaker")
+def get_sneaker(_id: str) -> Sneakers:
+    return container_controller.lamoda.get(_id)
