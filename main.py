923278be5a62ee9a9530a4b3d3e546da28d9e886
Candykiller16 <Candykiller16@mail.ru>
2022-09-19 13:40:36 +0300

    added data to mongo by container_controller

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from pprint import pprint
+import asyncio
 
 import uvicorn
-import asyncio
-from src.di import container_general, dao_container, container_parser, container_controller
+
+from src.di import container_general, container_parser, container_controller
 
 # db = dao_container.db
 #
@@ -22,9 +22,15 @@ if __name__ == '__main__':
     # print(collection.count_documents({}))
     # pprint(asyncio.run(container_parser.lamoda.get_all_data()))
 
-    container_controller.lamoda.collection.drop()
+    # container_controller.lamoda.collection.drop()
     # container_controller.lamoda.collection.insert_one(pylounge)
-    print(container_controller.lamoda.collection.count_documents({}))
+    data = asyncio.run(container_parser.lamoda.get_all_data())
+    for page in data:
+        container_controller.lamoda.create_list(page)
+
+    print(container_controller.lamoda.collection.count_documents({})) # 1500
+    container_controller.lamoda.collection.drop()
+    print(container_controller.lamoda.collection.count_documents({})) # 0
     uvicorn.run(
         container_general.app,
         host=container_general.config.service.host,
--- src/controllers/lamoda_controller.py
+++ src/controllers/lamoda_controller.py
@@ -6,3 +6,7 @@ class LamodaController:
     @property
     def collection(self):
         return self._collection
+
+    # CRUD operations for LamodaController to call them from ContainerController
+    def create_list(self, product_list: list):
+        return self.collection.insert_many(product_list)
